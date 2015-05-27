da3ff7a9d096f513b0595ee8bca6821946ee69d7
Drew Samsen <drewsamsen@gmail.com>
2015-05-27 16:27:27 -0700

    respond_to json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
-  include DeviseTokenAuth::Concerns::SetUserByToken
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  respond_to :json
+
+
 end
