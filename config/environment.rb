6184ffe38d5fce69def220a1645f7c2576932565
Drew Samsen <drewsamsen@gmail.com>
2015-05-27 17:47:51 -0700

    attempts to send auth emails

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Heroko does not automatically configure ActionMailer since this app is on the
+# Cedar stack (Bamboo stack is auto-setup)
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'], # 'app37253305@heroku.com',
+  :password       => ENV['SENDGRID_PASSWORD'], # 'l7ylyxtv4389',
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,11 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Go ahead and raise delivery errors in dev
+  config.action_mailer.raise_delivery_errors = true
+
+  # For Device
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
