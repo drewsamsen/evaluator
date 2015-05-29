68545ab5ba90da058b379d9f7fcd77274bdb4197
Drew Samsen <drewsamsen@gmail.com>
2015-05-29 14:24:01 -0700

    + ability to sort by 'score' or 'average' col on values list

--- ng-app/app/modules/value/value.controller.js
+++ ng-app/app/modules/value/value.controller.js
@@ -6,6 +6,10 @@ angular.module('valueMash')
 
   $scope.newValue = {};
 
+  // For sorting
+  $scope.sortDir = 1;
+  $scope.sortField;
+
   Value.getValues();
 
   $scope.createValue = function(data) {
@@ -24,6 +28,24 @@ angular.module('valueMash')
       form.$setPristine();
       form.$setUntouched();
     }
+  }
+
+  $scope.sort = function(field) {
+    // If we are sorting again by the currently sorted field, reverse directions
+    if ($scope.sortField === field) {
+      $scope.sortDir = $scope.sortDir * -1;
+
+    // Otherwise reset sort direction
+    } else {
+      $scope.sortDir = 1;
+    }
+
+    $scope.sortField = field;
+
+    Value.values.sort(function(a, b) {
+      return a[field] + ($scope.sortDir * b[field]);
+    });
+
   }
 
 });
